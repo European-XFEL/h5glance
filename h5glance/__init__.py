7b3f5577514fa4ccd8fdb73169ed8cd1c11f59fa
Thomas Kluyver <thomas@kluyver.me.uk>
2018-04-25 16:32:40 +0200

    Add function to install IPython formatter hooks for h5py

--- h5glance/__init__.py
+++ h5glance/__init__.py
@@ -1,1 +1,1 @@
-from .generate import H5Glance
+from .generate import H5Glance, install_ipython_h5py_display
--- h5glance/generate.py
+++ h5glance/generate.py
@@ -131,3 +131,12 @@ class H5Glance:
 
     def _repr_html_(self):
         return h5obj_to_html(self.obj)
+
+def install_ipython_h5py_display():
+    """Call inside IPython to install HTML views for h5py groups and files"""
+    from IPython import get_ipython
+    ip = get_ipython()
+    if ip is None:
+        raise EnvironmentError("This function is to be called in IPython")
+    html_formatter = ip.display_formatter.formatters['text/html']
+    html_formatter.for_type(h5py.Group, h5obj_to_html)
