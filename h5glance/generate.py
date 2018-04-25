e6bd8d6dad5cb779b74f5734c3be0a0ba7591503
Thomas Kluyver <thomas@kluyver.me.uk>
2018-04-25 16:26:05 +0200

    Details about each dataset

--- h5glance/generate.py
+++ h5glance/generate.py
@@ -1,7 +1,8 @@
 import h5py
-from htmlgen import Document, Element, Division, UnorderedList, Checkbox, Label, ListItem
+from htmlgen import (Document, Element, Division, UnorderedList, Checkbox,
+                     Label, ListItem, html_attribute, Span,
+                     )
 from pathlib import Path
-import sys
 
 _PKGDIR = Path(__file__).parent
 
@@ -9,6 +10,43 @@ class Style(Element):
     def __init__(self, styles):
         super().__init__('style')
         self.children.append_raw(styles)
+
+class Abbreviation(Element):
+    def __init__(self, *content, title=""):
+        super().__init__("abbr")
+        self.extend(content)
+        self.title = title
+
+    title = html_attribute('title')
+
+class Code(Element):
+    """An HTML in-line <code> element.
+    """
+    def __init__(self, *content):
+        super().__init__("code")
+        self.extend(content)
+
+dtype_kind_to_descr = {
+    "u": "unsigned integer",
+    "i": "signed integer",
+    "f": "floating point",
+    "c": "complex floating point",
+    "b": "boolean",
+    "O": "object (e.g. string)",
+}
+
+def make_dtype_abbr(dtobj):
+    desc = ""
+    if dtobj.kind in "uifc":
+        desc += "{}-bit ".format(dtobj.itemsize * 8)
+    try:
+        desc += dtype_kind_to_descr[dtobj.kind]
+    except KeyError:
+        pass
+    code = Code(dtobj.str)
+    if desc:
+        return Abbreviation(code, title=desc)
+    return code
 
 def make_list(*items):
     ul = UnorderedList()
@@ -27,7 +65,15 @@ def checkbox_w_label(label_content):
     return [c, l]
 
 def item_for_dataset(name, ds):
-    return ListItem(name)
+    shape = " × ".join(str(n) for n in ds.shape)
+    namespan = Span(name)
+    namespan.add_css_classes("h5glance-dataset-name")
+    li = ListItem(
+        namespan, ": ",
+        shape, " entries, dtype: ", make_dtype_abbr(ds.dtype)
+    )
+    li.add_css_classes("h5glance-dataset")
+    return li
 
 def item_for_group(gname, grp):
     subgroups, datasets = [], []
