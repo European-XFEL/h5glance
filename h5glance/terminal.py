96cb0b10212e885bf7121b8185b679a0af9e7a67
Thomas Kluyver <thomas.kluyver@xfel.eu>
2018-09-11 16:04:08 +0200

    Allow selecting a part of the file to display

--- h5glance/terminal.py
+++ h5glance/terminal.py
@@ -11,6 +11,8 @@ from subprocess import run
 import sys
 
 def detail_for(obj):
+    """Detail for an HDF5 object, to display by its name in the tree view.
+    """
     if isinstance(obj, h5py.Dataset):
         detail = '\t[{dt}: {shape}]'.format(
             dt=obj.dtype.name,
@@ -35,19 +37,32 @@ def print_paths(group, prefix='', file=None):
             print_paths(obj, prefix + ('  ' if islast else '│ '), file=file)
 
 def page(text):
+    """Display text in a terminal pager
+
+    Respects the PAGER environment variable if set.
+    """
     pager_cmd = shlex.split(os.environ.get('PAGER') or 'less -r')
     run(pager_cmd, input=text.encode('utf-8'))
 
-def display_tree(file, groupname=None):
+def display_h5_obj(file: h5py.File, path=None):
+    """Display information on an HDF5 file, group or dataset
+
+    This is the central function for the h5glance command line tool.
+    """
     sio = io.StringIO()
-    if groupname:
-        print(file.filename + '/' + groupname, file=sio)
-        group = file[groupname]
+    if path:
+        print(file.filename + '/' + path.lstrip('/'), file=sio)
+        obj = file[path]
     else:
         print(file.filename, file=sio)
-        group = file
-    print_paths(group, file=sio)
+        obj = file
 
+    if isinstance(obj, h5py.Group):
+        print_paths(obj, file=sio)
+    else:
+        print(detail_for(obj), file=sio)
+
+    # If the output has more lines than the terminal, display it in a pager
     output = sio.getvalue()
     if sys.stdout.isatty():
         nlines = len(output.splitlines())
@@ -57,18 +72,74 @@ def display_tree(file, groupname=None):
 
     print(output)
 
+class H5Completer:
+    """Readline tab completion for paths inside an HDF5 file"""
+    def __init__(self, file: h5py.File):
+        self.file = file
+        self.cache = (None, [])
+
+    def completions(self, text: str):
+        if text == self.cache[0]:
+            return self.cache[1]
+        prev_path, _, prefix = text.rpartition('/')
+        group = self.file
+        if prev_path:
+            group = self.file[prev_path]
+            prev_path += '/'
+        res = [prev_path + k + ('/' if isinstance(v, h5py.Group) else '')
+               for (k, v) in group.items()
+               if k.lower().startswith(prefix.lower())]
+        self.cache = (text, res)
+        return res
+
+    def rlcomplete(self, text: str, state: int):
+        # print(repr(text), state)
+        try:
+            res = self.completions(text)
+        except Exception as e:
+            #print(e)
+            return None
+        # print(repr(text), len(res))
+        if state >= len(res):
+            return None
+        return res[state]
+
+def prompt_for_path(filename):
+    """Prompt the user for a path inside the HDF5 file"""
+    import readline
+    with h5py.File(filename, 'r') as f:
+        compl = H5Completer(f)
+        readline.set_completer(compl.rlcomplete)
+        readline.set_completer_delims('')
+        readline.parse_and_bind("tab: complete")
+        while True:
+            res = input("Object path: {}/".format(filename))
+            if f.get(res) is not None:
+                print()
+                return res
+            print("No object at", repr(res))
+
+
 def main(argv=None):
     ap = argparse.ArgumentParser(prog="h5glance",
              description="View HDF5 file structure in the terminal")
-    ap.add_argument("input", help="HDF5 file to view", type=Path)
+    ap.add_argument("file", help="HDF5 file to view", type=Path)
+    ap.add_argument("path", nargs='?',
+        help="Object to show within the file, or '-' to prompt for a name"
+    )
+
     args = ap.parse_args(argv)
 
-    if not args.input.is_file():
-        print("Not a file:", args.input)
+    if not args.file.is_file():
+        print("Not a file:", args.file)
         sys.exit(2)
-    elif not h5py.is_hdf5(args.input):
-        print("Not an HDF5 file:", args.input)
+    elif not h5py.is_hdf5(args.file):
+        print("Not an HDF5 file:", args.file)
         sys.exit(2)
 
-    with h5py.File(args.input, 'r') as f:
-        display_tree(f)
+    path = args.path
+    if path == '-':
+        path = prompt_for_path(args.file)
+
+    with h5py.File(args.file, 'r') as f:
+        display_h5_obj(f, path)
