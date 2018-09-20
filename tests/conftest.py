e2dcbc2e54310ac49c5229d35d8401681b2253fe
Thomas Kluyver <thomas.kluyver@xfel.eu>
2018-09-20 15:49:03 +0200

    Test attribute display

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import h5py
+import numpy
 import os
 import pytest
 from tempfile import TemporaryDirectory
@@ -10,4 +11,6 @@ def simple_h5_file():
         f.create_dataset("/group1/subgroup1/dataset1", (200,), dtype='u8')
         f.create_dataset("/group1/subgroup1/dataset2", (2, 128, 500), dtype='f4')
         f.create_dataset("/group1/subgroup2/dataset1", (12,), dtype='i2')
+        f['group1'].attrs['string'] = 'foo'
+        f['group1'].attrs['array'] = numpy.zeros((3, 4))
         yield f
--- tests/test_terminal.py
+++ tests/test_terminal.py
@@ -10,6 +10,19 @@ def test_tree_view(simple_h5_file):
     assert 'uint64' in out
     assert '128 × 500' in out
 
+def test_attributes(simple_h5_file):
+    r1, c1 = terminal.object_tree_node(simple_h5_file['group1'], 'group1')
+    assert '2 attributes' in r1
+    assert 'attributes' not in c1[0][0]
+
+    r2, c2 = terminal.object_tree_node(simple_h5_file['group1'], 'group1',
+                                       expand_attrs=True)
+    assert 'attributes' not in r2
+    assert '2 attributes' in c2[0][0]
+    attr_lines = {node[0] for node in c2[0][1]}
+    assert len(attr_lines) == 2
+
+
 def test_dataset_info(simple_h5_file):
     sio = io.StringIO()
     terminal.print_dataset_info(simple_h5_file["/group1/subgroup1/dataset2"], file=sio)
