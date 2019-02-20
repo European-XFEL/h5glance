d93155137c058002c031f67c9db8f4f80a41ee5e
Thomas Kluyver <thomas@kluyver.me.uk>
2019-02-20 19:39:53 +0000

    First try at zsh completion

---
+++ h5glance/completion.zsh
@@ -0,0 +1,28 @@
+#compdef _h5glance h5glance
+
+function _h5glance {
+    local curcontext="$curcontext"
+    local context state state_descr line
+    typeset -A opt_args
+
+
+    _arguments -C \
+        "-h[Show help information]" \
+        "--help[Show help information]" \
+        "--version[Show version number]" \
+        "--attrs[Show attributes of groups]" \
+        ":HDF5 file:_files" \
+        ":path in file:->infile"
+
+    case "$state" in
+        infile)
+            declare -a matching_paths
+            matching_paths=($(_h5glance_complete_infile "${line[1]}" "${line[2]}"))
+            _describe "paths in file" matching_paths
+            ;;
+    ecas
+}
+
+function _h5glance_infile {
+
+}
