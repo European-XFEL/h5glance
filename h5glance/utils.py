578509ae9793194889573eb97ad1d0c94a6dd935
Valentin Valls <valentin.valls@esrf.fr>
2019-04-16 15:08:04 +0200

    Avoid to load h5pyd while it is not needed

--- h5glance/utils.py
+++ h5glance/utils.py
@@ -2,34 +2,47 @@
 """
 import h5py
 
-try:
+_mapping = {}
+_mapping[h5py.File] = "file"
+_mapping[h5py.Group] = "group"
+_mapping[h5py.Dataset] = "dataset"
+
+
+def register_h5pyd():
     import h5pyd
-except ImportError:
-    h5pyd = None
+    _mapping[h5pyd.File] = "file"
+    _mapping[h5pyd.Group] = "group"
+    _mapping[h5pyd.Dataset] = "dataset"
+
+
+def check_class(obj_class):
+    """Check registered class"""
+    lib = obj_class.__module__.split(".")[0]
+    if lib == "h5pyd":
+        register_h5pyd()
+    else:
+        _mapping[obj_class] = None
 
 
 def is_file(obj):
     """Returns true if the object is a h5py-like file."""
-    if isinstance(obj, h5py.File):
-        return True
-    if h5pyd is not None and isinstance(obj, h5pyd.File):
-        return True
-    return False
+    obj_class = obj.__class__
+    if obj_class not in _mapping:
+        check_class(obj_class)
+    return _mapping.get(obj_class, None) == "file"
 
 
 def is_dataset(obj):
     """Returns true if the object is a h5py-like dataset."""
-    if isinstance(obj, h5py.Dataset):
-        return True
-    if h5pyd is not None and isinstance(obj, h5pyd.Dataset):
-        return True
-    return False
+    obj_class = obj.__class__
+    if obj_class not in _mapping:
+        check_class(obj_class)
+    return _mapping.get(obj_class, None) == "dataset"
 
 
 def is_group(obj):
     """Returns true if the object is a h5py-like group."""
-    if isinstance(obj, h5py.Group):
-        return True
-    if h5pyd is not None and isinstance(obj, h5pyd.Group):
-        return True
-    return False
+    obj_class = obj.__class__
+    if obj_class not in _mapping:
+        check_class(obj_class)
+    return _mapping.get(obj_class, None) in ["file", "group"]
