e94890d761ffd96f1b9ef75621acec9918b22fa6
Valentin Valls <valentin.valls@esrf.fr>
2019-04-16 14:51:45 +0200

    Bind h5pyd

--- h5glance/utils.py
+++ h5glance/utils.py
@@ -2,17 +2,34 @@
 """
 import h5py
 
+try:
+    import h5pyd
+except ImportError:
+    h5pyd = None
+
 
 def is_file(obj):
     """Returns true if the object is a h5py-like file."""
-    return isinstance(obj, h5py.File)
+    if isinstance(obj, h5py.File):
+        return True
+    if h5pyd is not None and isinstance(obj, h5pyd.File):
+        return True
+    return False
 
 
 def is_dataset(obj):
     """Returns true if the object is a h5py-like dataset."""
-    return isinstance(obj, h5py.Dataset)
+    if isinstance(obj, h5py.Dataset):
+        return True
+    if h5pyd is not None and isinstance(obj, h5pyd.Dataset):
+        return True
+    return False
 
 
 def is_group(obj):
     """Returns true if the object is a h5py-like group."""
-    return isinstance(obj, h5py.Group)
+    if isinstance(obj, h5py.Group):
+        return True
+    if h5pyd is not None and isinstance(obj, h5pyd.Group):
+        return True
+    return False
